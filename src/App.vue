22104d02ba41e8ed2e52df0d039ed5a08fb65d93
amankr96 <aman.kr96@gmail.com>
2020-03-25 13:14:12 +0530

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2>{{ quoteTitle }}</h2>
+          <p>A wonderful quote !!</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote';
+  export default {
+    data: function() {
+      return {
+        quoteTitle: 'This is a Quote !!!'
+      }
+    },
+    components: {
+      appQuote: Quote
+    }
+  }
+  </script>
+
+ <style scoped>
+ </style>

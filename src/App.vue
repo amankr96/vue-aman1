fc482a6aa4813b9027d28c99ca826175c45262d3
amankr96 <aman.kr96@gmail.com>
2020-03-27 19:55:40 +0530

    understanding dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,40 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <app-quote>
-          <h2>{{ quoteTitle }}</h2>
-          <p>A wonderful quote !!</p>
-        </app-quote>
+        <button @click="selectedComponent='appQuote'">Quote</button>
+        <button @click="selectedComponent='appAuthor'">Author</button>
+        <button @click="selectedComponent='appNew'">New</button>
+        <hr>
+        <p>{{selectedComponent}}</p>
+        <keep-alive>
+        <component :is="selectedComponent">
+          <p slot="content">A wonderful quote !!</p>
+        </component>
+        </keep-alive>
+        <!-- <app-quote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p slot="content">A wonderful quote !!</p>
+        </app-quote> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Quote from './components/Quote';
+import Quote from './components/Quote.vue';
+import Author from './components/Author.vue';
+import New from './components/New.vue';
   export default {
     data: function() {
       return {
-        quoteTitle: 'This is a Quote !!!'
+        quoteTitle: 'This is a Quote !!!',
+        selectedComponent: 'appQuote'
       }
     },
     components: {
-      appQuote: Quote
+      appQuote: Quote,
+      appAuthor:Author,
+      appNew: New
     }
   }
   </script>
--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-      <slot></slot>
+      <div class="title">
+      <slot name="title"></slot>
+      <span style="color:rgb(0,0,0)"><slot name="subtitle">The subtitle</slot></span>
+      </div>
+      <hr>
+      <div class="content">
+        <slot name="content"></slot>
+      </div>
     </div>
 </template>
 
@@ -12,11 +19,18 @@ export default{
 
 <style scoped>
 div {
-    border: 1px solid #ccc;
+    border: 1px solid rgb(236, 24, 24);
     box-shadow: 1px 1px 2px black;
+    background-color: rgb(201, 179, 199);
     padding: 30px;
     margin: auto;
     text-align: center;
 }
+
+.title
+{
+  font-style: italic;
+}
+
 
 </style>
